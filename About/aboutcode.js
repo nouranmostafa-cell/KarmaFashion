cdb73ac3ffef7ae9391eaee650c7864c8371b482
nouranmostafa-cell <nouranmostafa1234@gmail.com>
2023-09-18 09:54:37 +0200

    Update aboutcode.js

--- About/aboutcode.js
+++ About/aboutcode.js
@@ -1,6 +1,3 @@
-window.onload = ()=>{
-    aboutAnim.restart();
-}
 
 const aboutAnim = gsap.timeline({paused: true ,repeat:-1,repeatDelay: 0.1,yoyo: true}); 
 aboutAnim.to("#leaf", 1.2, { rotation:10, transformOrigin:"center bottom", ease: Power4.linear},0); /* https://greensock.com/forums/topic/7949-how-to-set-rotate-an-object-with-origin/ */  /*https://greensock.com/docs/v3/GSAP/gsap.fromTo()*/
@@ -10,3 +7,6 @@ aboutAnim.to("#womanHand", 1.3, {rotation:-15, transformOrigin:"right bottom", e
 aboutAnim.to("#womanFace", 1, {rotation:-10, transformOrigin:"center bottom", ease: Power4.linear},0); 
 aboutAnim.to("#man2Face", 1, {rotation:15, transformOrigin:"center bottom", ease: Power4.linear},0); 
 aboutAnim.to("#man1Face", 1, {rotation:15, transformOrigin:"center bottom", ease: Power4.linear},0); 
+
+
+aboutAnim.restart();
